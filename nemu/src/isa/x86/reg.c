2d4baaf9fe17e6ac15d9db0659c2a47f8ed795c8
tracer-ics2019 <tracer@njuics.org>
2020-02-22 15:56:02 +0000

    >  compile 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  15:56:02 up  7:42,  1 user,  load average: 0.01, 0.01, 0.00 8f9bf2cb6f79aaf4143bd709b9ef7c4fbd1c3807

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -43,7 +43,7 @@ void reg_test() {
 void isa_reg_display() {
   for(int index = 0; index < 8; index++)
     printf("| %-5s\t%-15d| %-5s\t%-15d| %-5s\t%-15d|\n", regsl[index], reg_l(index), regsw[index], reg_w(index), regsb[index], reg_b(index));
-  printf("eip\t%d\n", cpu.pc);
+  printf("pc\t%d\n", cpu.pc);
 }
 
 uint32_t isa_reg_str2val(const char *s, bool *success) {
