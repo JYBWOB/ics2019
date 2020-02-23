b80b5532dd19e78c3ed9fc997284152b2da8dde3
tracer-ics2019 <tracer@njuics.org>
2020-02-23 16:04:57 +0000

    >  compile 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  16:04:57 up  4:47,  1 user,  load average: 0.00, 0.00, 0.00 7a1fdb9cf22d708ba533230cba7e294cf3b78b4

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -115,6 +115,7 @@ static int cmd_scan(char *args) {
     Log("Expr evaluation failed.");
     assert(0);
   }
+  printf("calculate result: %x", expr_val);
   uint32_t val;
   for (int i = 0; i < scan_num; ++i) {
     val = vaddr_read(expr_val + (i << 2), 4);
