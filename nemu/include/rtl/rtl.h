a0894d3fa09d1a4cae864f8b6206630921d56af8
tracer-ics2019 <tracer@njuics.org>
2020-04-01 15:06:10 +0000

    >  compile 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  15:06:10 up  2:30,  1 user,  load average: 0.00, 0.00, 0.00 1514aab0405655c230ddbde5f13658f1c790e0fb

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -137,7 +137,20 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  // TODO();
+  if(width == 4)
+		*dest = *src1;
+	else if(width == 2)
+	{
+		uint16_t result = *src1;
+		int16_t tmp = result ;
+		*dest = tmp;
+	}
+	else if(width == 1){
+		uint8_t result = *src1;
+		int8_t tmp = result ;
+		*dest = tmp;
+	}
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
