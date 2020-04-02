a786c2c818dbeabf2796b7672c55bf18946c0a52
tracer-ics2019 <tracer@njuics.org>
2020-04-02 14:21:06 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  14:21:06 up  9:40,  1 user,  load average: 0.01, 0.10, 0.09 6bbbb674b1188064a2cea14b9928e2ab565264eb

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -2,14 +2,15 @@
 #include <amdev.h>
 #include <nemu.h>
 
+#define I8042_DATA_PORT 0x60
 #define KEYDOWN_MASK 0x8000
 
 size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      kbd->keydown = inl(I8042_DATA_PORT);
+      kbd->keycode = (kbd->keydown & KEYDOWN_MASK)? 1:0;
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
