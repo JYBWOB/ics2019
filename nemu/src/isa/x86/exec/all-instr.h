bbece061b2fc0a397e8a17107ee7c54334c93304
tracer-ics2019 <tracer@njuics.org>
2020-04-02 11:33:01 +0000

    >  compile 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  11:33:01 up  6:52,  1 user,  load average: 0.00, 0.00, 0.00 3639b074f54997e2a7e47855f29c87f13b16e647

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -43,6 +43,7 @@ make_EHelper(and);
 make_EHelper(or);
 make_EHelper(test);
 make_EHelper(setcc);
+make_EHelper(sar);
 
 // special.c
 make_EHelper(nop);
--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -50,8 +50,17 @@ make_EHelper(or) {
 }
 
 make_EHelper(sar) {
-  TODO();
+  // TODO();
   // unnecessary to update CF and OF in NEMU
+  if (id_dest->width == 1) {
+		id_dest->val = (int8_t)id_dest->val;
+	}
+	else if (id_dest->width == 2) {
+		id_dest->val = (int16_t)id_dest->val;
+	}
+	rtl_sar(&t2, &id_dest->val, &id_src->val);
+	operand_write(id_dest, &t2);
+	rtl_update_ZFSF(&t2, id_dest->width);
 
   print_asm_template2(sar);
 }
