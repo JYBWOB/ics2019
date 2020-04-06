ff6928f92a21395fc04e662d27be986ab3b703e2
tracer-ics2019 <tracer@njuics.org>
2020-04-06 14:32:20 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  14:32:20 up  3:33,  1 user,  load average: 0.10, 0.25, 0.24 61dbf309e15298e6bd776b8a4949309647a60cc2

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,143 +3,117 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-char* get_ch(char *dest, int num, int n) {
-	char number[20];
-	for (int j=0;j<10;j++) {number[j]=j+'0';}
-	number[10] = 'a'; number[11] = 'b'; number[12] = 'c'; number[13] = 'd'; number[14] = 'e'; number[15] = 'f';
-	int l=0;
-	int flag=0;
-	if(num==0) {
-		dest[0]='0';
-		dest[1]='\0';
-		return dest;
-	}
-	if(num<0) {
-		flag=1;
-		l++;
-		num=-num;
-	}
-	int temp=num;
-	while(temp>0) {
-		l++;
-		temp=temp/n;
-	}
-	for (int a=0; a<l; a++) {
-		int x=num%n;
-		assert(x<16);
-		dest[l-a-1]=number[x];
-		num=num/n;
-	}
-	if(flag) {dest[0]='-';}
-	dest[l]='\0';
-	return dest;
-}
-
 int printf(const char *fmt, ...) {
-	va_list ap;
-  	va_start(ap,fmt);
-  	int sum=0;
-  	int i=0;
-  	int len=strlen(fmt);
-	while(i<len) {
-    if(fmt[i]=='%') {
-      int num=0;
-      char ls[50]="";
-      switch(fmt[i+1]) {
-        case 'd': {
-	  		num=va_arg(ap,int);
-	  		get_ch(ls, num, 10);
-	  		for(int j = 0; j < strlen(ls); j++) {_putc(ls[j]);}
-	  		sum += strlen(ls);	
-			i+=2;
-        } break;
-		case 's': {
-	  		strcpy(ls,va_arg(ap, char*));
-	  		for(int j = 0; j < strlen(ls); j++) _putc(ls[j]);
-	  		sum += strlen(ls);
-			i+=2;
-		} break;
-		case '0': {
-			num=va_arg(ap,int);
-	  		get_ch(ls, num, 10);
-			int width = fmt[i+2]-'0';
-			for(int i = strlen(ls); i < width; i++) {
-				sum++;
-				_putc('0');
-			}
-	  		for(int j = 0; j < strlen(ls); j++) {_putc(ls[j]);}
-	  		sum += strlen(ls);	
-			i+=4;
-		} break;
-		case 'p': {
-				num=va_arg(ap, int);
-				get_ch(ls, num, 16);
-				_putc('0'); _putc('x');
-				sum += 2;
-				for(int j = 0; j < strlen(ls); j++) _putc(ls[j]);
-				sum+=strlen(ls);
-				i += 2;
-		} break;
-		default: {
-			i = len;
-		}
-	  } 
-	}
-    else {
-      	_putc(fmt[i]);
-		sum++;  	
-      	i++;
-  	}
- }
-  va_end(ap);
-  return sum; 
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
+  va_list args;
+  int i,len;
+  char out[200];
+  va_start(args,fmt);
+  vsprintf(out,fmt,args);
+  va_end(args);
+  len = strlen(out);
+  for(i = 0;i < len;i ++)
+  {
+    _putc(out[i]);
+  }
   return 0;
 }
 
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  char c;
+  char *str = out;
+  const char *tmp;
+  char num_s[100];
+  int i,j,len,num;
+  int flag,field_width;
+
+  for(;*fmt; fmt++)
+  {
+      if(*fmt != '%')
+      {
+	  *str++ = *fmt;
+	  continue;
+      }
+
+      flag = 0;
+      fmt++;
+      while(*fmt == ' ' || *fmt == '0')
+      {
+	if(*fmt == ' ')  flag |= 8;
+	else if(*fmt == '0') flag |= 1;
+	fmt++;
+      }
+
+      field_width = 0;
+      if(*fmt >= '0' && *fmt <= '9')
+      {
+	      while(*fmt >= '0' && *fmt <= '9')
+	      {
+		      field_width = field_width*10 + *fmt -'0';
+		      fmt++;
+	      }
+      }
+      else if(*fmt == '*')
+      {
+	      fmt++;
+	      field_width = va_arg(ap,int);
+      }
+      switch(*fmt)
+      {
+	  case 's':
+	      tmp = va_arg(ap,char *);
+	      len = strlen(tmp);
+	      for(i = 0;i < len;i ++)
+	      {
+		   *str++ = *tmp++;
+	      }
+	      continue;
+	  case 'd': break;
+      }
+
+      num = va_arg(ap,int);
+      j = 0;
+      if(num == 0)
+      {
+	  num_s[j++] = '0';
+      }
+      else
+      {
+	  if(num < 0)
+	  {
+	      *str++ = '-';
+	      num = -num;
+	  }
+	  while(num)
+	  {
+	      num_s[j++] = num%10 + '0';
+	      num /= 10;
+	  }
+      }
+      if(j < field_width)
+      {
+	      num = field_width - j;
+	      c = flag & 1 ? '0' : ' ';
+	      while(num--)
+	      {
+		      *str++ = c;
+	      }
+      }
+      while(j--)
+      {
+	  *str++ = num_s[j];
+      }
+  }
+  *str = '\0';
+  return 0;
+}
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap,fmt);
-  int sum=0;
-  int i=0;
-  int len=strlen(fmt);
-  out[0]='\0';
-  while(i<len) {
-    if(fmt[i]=='%') {
-      int num=0;
-      char ls[50]="";
-      switch(fmt[i+1]) {
-        case 'd': {
-	  		num=va_arg(ap,int);
-	  		get_ch(ls, num, 10);
-	  		strcat(out,ls);
-	  		sum+=strlen(ls);	
-        } break;
-		case 's': {
-	  		strcpy(ls,va_arg(ap, char*));
-	  		strcat(out,ls);
-	  		sum += strlen(ls);
-		} break;
-		default: {
-			printf("sprintf fault \n");
-		}
-	  } 
-      i+=2;
-	}
-    else {
-      	sum++;
-      	char s[2];
-      	s[0]=fmt[i];
-      	s[1]='\0';
-      	strcat(out,s);
-      	i++;
-  	}
- }
-  va_end(ap);
-  return sum; 
+  va_list args;
+  int  val;
+  va_start(args,fmt);
+  val = vsprintf(out,fmt,args);
+  va_end(args);
+  return val;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
