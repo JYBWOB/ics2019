0d843ddb7db013b10aac8b02bc1b064a5759e9ed
tracer-ics2019 <tracer@njuics.org>
2020-04-06 15:10:37 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  15:10:37 up  4:11,  1 user,  load average: 0.86, 0.35, 0.18 b33d62e61d09c4dce873067d078e3af11cb5243

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -15,8 +15,8 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
       // info->width = SCREEN_W;
       // info->height = SCREEN_H;
-      info->width = 3;
-      info->height = 3;
+      info->width = 300;
+      info->height = 300;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
