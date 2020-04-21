4dbe8c611e0f3129f2daa6b1409d79b9234a9f64
tracer-ics2019 <tracer@njuics.org>
2020-04-21 03:00:07 +0000

    >  compile 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  03:00:07 up  1:33,  1 user,  load average: 0.00, 0.00, 0.00 7875da2b7c14e4b48b8cd9c5afde2e23eab73a5e

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -23,7 +23,7 @@ static inline void rtl_sr(int r, const rtlreg_t* src1, int width) {
   }
 }
 
-inline void rtl_push(const rtlreg_t* src1) {
+void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   cpu.esp -= 4;
