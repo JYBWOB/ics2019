40e67dc17f999b5660eb4dde078477ee01b22ab3
tracer-ics2019 <tracer@njuics.org>
2020-05-12 08:30:01 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  08:30:01 up  1:25,  1 user,  load average: 0.34, 0.11, 0.03 399cd4f250b4dfee1c7bdeca2e07d697333f35

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include "proc.h"
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __ISA_AM_NATIVE__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,9 +10,22 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  // TODO();
+  Elf_Ehdr ehdr;
+  ramdisk_read((void *)&ehdr, 0, sizeof(Elf_Ehdr));
+  for (size_t i = 0; i < ehdr.e_phnum; ++i) {
+    Elf_Phdr phdr;
+    ramdisk_read((void *)&phdr, ehdr.e_ehsize, sizeof(Elf_Phdr)*i);
+    if (phdr.p_type == PT_LOAD) {
+      ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_memsz);
+      memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+    }
+  }
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
