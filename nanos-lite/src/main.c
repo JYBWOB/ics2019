275d93bc4bd00c5b47364c3ab0099ed3935ab70e
tracer-ics2019 <tracer@njuics.org>
2020-05-15 05:01:51 +0000

    >  compile 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  05:01:51 up  1:19,  1 user,  load average: 0.00, 0.00, 0.00 eaf0c5f249fa4c479c899ce9dd34dfa15de00eac

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -10,7 +10,9 @@ void init_proc(void);
 extern const char logo[];
 
 int main() {
+  printf("11111111111111");
   printf("%s", logo);
+  printf("22222222222222");
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
