0ffb5ddbe89028693eeed07f0836d484cd203869
tracer-ics2019 <tracer@njuics.org>
2020-05-17 05:22:54 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  05:22:54 up  1:27,  1 user,  load average: 0.08, 0.02, 0.01 29f31788dba30da46e765c49ef1065187d8150ec

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,14 @@
 #include "common.h"
 #include "syscall.h"
 
-// void naive_uload(PCB *pcb, const char *filename);
-
-// static inline int32_t sys_execve(const char *pathname, char *const argv[], char *const envp[]) {
-//   naive_uload(NULL, pathname);
-//   return 0;
-// }
+static inline int32_t sys_write(int fd, void *buf, size_t len) {
+  if (fd==1 || fd==2) {
+    for (size_t i=0; i<len; i++) {
+      _putc(*(char*)(buf+i));
+    }
+  }
+  return len;
+}
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
@@ -18,6 +20,10 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
     case SYS_exit: _halt(a[1]); break;
     case SYS_yield: _yield(); c->GPRx = 0; break;
+    case SYS_write: 
+      Log("write\n");
+      c->GPRx = sys_write(a[1], (void *)a[2], (size_t)a[3]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
