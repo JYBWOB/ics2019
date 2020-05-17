2fcbfe4a39fb624a17bc31fe0c0fe29cbbf5d2ee
tracer-ics2019 <tracer@njuics.org>
2020-05-17 05:09:48 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  05:09:48 up  1:14,  1 user,  load average: 0.00, 0.00, 0.00 625a233ca50242c26747083ed82487425abb0d2e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,23 @@
 #include "common.h"
 #include "syscall.h"
 
+// void naive_uload(PCB *pcb, const char *filename);
+
+// static inline int32_t sys_execve(const char *pathname, char *const argv[], char *const envp[]) {
+//   naive_uload(NULL, pathname);
+//   return 0;
+// }
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
+    // case SYS_exit: sys_execve(a[1], NULL, NULL); break;
+    case SYS_yield: _yield(); c->GPRx = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
