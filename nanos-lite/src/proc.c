a243d202e673ea77a3a0ca87fc6993fcf415e451
tracer-ics2019 <tracer@njuics.org>
2020-05-21 06:41:47 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  06:41:47 up  2:00,  1 user,  load average: 0.13, 0.10, 0.09 d4a3a1fc95bf5503a690cc1359ba0f282eb00e4b

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,7 @@ void init_proc() {
 
   // load program here
   // naive_uload(NULL, NULL);
-  naive_uload(NULL, "/bin/dummy");
+  naive_uload(NULL, "/bin/text");
   // Log("proc finished!!!!!!!!!!!!!!!!!!!!");
 }
 
