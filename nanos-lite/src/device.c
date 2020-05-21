1c230e1ec7e48f326530a13ac4ff4a87d8dfa362
tracer-ics2019 <tracer@njuics.org>
2020-05-21 07:09:09 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  07:09:09 up  2:27,  1 user,  load average: 0.08, 0.04, 0.05 2fb2097e7f9e0382e8ede758671025618593b

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -85,4 +85,5 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", screen_width(), screen_height());
 }
