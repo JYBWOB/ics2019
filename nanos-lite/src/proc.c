5f21109ad865b671777a59be687d782029e4ae8c
tracer-ics2019 <tracer@njuics.org>
2020-05-23 11:47:49 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  11:47:49 up  1:09,  1 user,  load average: 0.08, 0.04, 0.01 8ba15c63e105da768a5f51d174fbb5d32b16156d

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,7 @@ void init_proc() {
 
   // load program here
   // naive_uload(NULL, NULL);
-  naive_uload(NULL, "/bin/events");
+  naive_uload(NULL, "/bin/bmptest");
 }
 
 _Context* schedule(_Context *prev) {
