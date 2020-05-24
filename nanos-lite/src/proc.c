992c1a39520be571b795e9c243f40ebc4c689acf
tracer-ics2019 <tracer@njuics.org>
2020-05-24 08:51:32 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  08:51:32 up  2:09,  1 user,  load average: 0.01, 0.03, 0.02 bec93e4c3de31bf1c56c1deb714960ad7a3686d0

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   // context_kload(&pcb[0], (void*)hello_fun);
-  context_uload(&pcb[1], "/bin/bmptest");
+  context_uload(&pcb[1], "/bin/text");
 
   switch_boot_pcb();
 
