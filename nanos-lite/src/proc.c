534f6285414afe6e6e629939332917e2159447fd
tracer-ics2019 <tracer@njuics.org>
2020-05-24 08:41:27 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  08:41:27 up  1:59,  1 user,  load average: 0.12, 0.06, 0.04 e266ff555faf13e3f3b6558f1bddda7689740857

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -39,7 +39,7 @@ void init_proc() {
 int cnt = 0;
 _Context* schedule(_Context *prev) {
   // return NULL;
-  // current->cp = prev;
+  current->cp = prev;
   // current = &pcb[0];
   // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   // return current->cp;
