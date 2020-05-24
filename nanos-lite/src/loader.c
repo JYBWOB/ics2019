45c3cc6d52d9967eaacd9ea69636bc7e7dccad73
tracer-ics2019 <tracer@njuics.org>
2020-05-24 09:35:58 +0000

    >  compile 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  09:35:58 up  2:54,  1 user,  load average: 0.14, 0.09, 0.04 fd68bad3b624dc5e288362fd4aaceb788a22f5d5

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,38 +15,38 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
-    int fd = fs_open(filename, 'r', 0);
-    Elf_Ehdr E_hdr;
-    Elf_Phdr P_hdr;
-    fs_read(fd, &E_hdr, sizeof(Elf_Ehdr));
+  int fd = fs_open(filename, 'r', 0);
+  Elf_Ehdr E_hdr;
+  Elf_Phdr P_hdr;
+  fs_read(fd, &E_hdr, sizeof(Elf_Ehdr));
 
-    for (int i = 0; i < E_hdr.e_phnum; i++) {
-        fs_lseek(fd, E_hdr.e_phoff + i * E_hdr.e_phentsize, SEEK_SET);
-        fs_read(fd, &P_hdr, E_hdr.e_phentsize);
-        if (P_hdr.p_type == PT_LOAD) {
-            fs_lseek(fd, P_hdr.p_offset, SEEK_SET);
-            void* vaddr = (void*)P_hdr.p_vaddr;
-            uint32_t i = 0;
-            for (; i < P_hdr.p_filesz; i += PGSIZE) {
-                void* paddr = new_page(1);
-                uint32_t sz = (P_hdr.p_filesz - i >= PGSIZE) ? PGSIZE : (P_hdr.p_filesz - i);
-                _map(&pcb->as, vaddr + i, paddr, 0);
-                fs_read(fd, paddr, sz);
-            }
-            vaddr = vaddr + i;
-            i = 0;
-            uint32_t zero_size = P_hdr.p_memsz - P_hdr.p_filesz;
-            for (; i < zero_size;i += PGSIZE){
-                void* paddr = new_page(1);
-                uint32_t sz = (zero_size - i >= PGSIZE) ? PGSIZE : (zero_size - i);
-                _map(&pcb->as, vaddr + i, paddr, 0);
-                memset(paddr, 0, sz);
-            }
-            pcb->max_brk = PGROUNDUP((uint32_t)vaddr + i);
-        }
+  for (int i = 0; i < E_hdr.e_phnum; i++) {
+    fs_lseek(fd, E_hdr.e_phoff + i * E_hdr.e_phentsize, SEEK_SET);
+    fs_read(fd, &P_hdr, E_hdr.e_phentsize);
+    if (P_hdr.p_type == PT_LOAD) {
+      fs_lseek(fd, P_hdr.p_offset, SEEK_SET);
+      void* vaddr = (void*)P_hdr.p_vaddr;
+      uint32_t i = 0;
+      for (; i < P_hdr.p_filesz; i += PGSIZE) {
+        void* paddr = new_page(1);
+        uint32_t sz = (P_hdr.p_filesz - i >= PGSIZE) ? PGSIZE : (P_hdr.p_filesz - i);
+        _map(&pcb->as, vaddr + i, paddr, 0);
+        fs_read(fd, paddr, sz);
+      }
+      vaddr = vaddr + i;
+      i = 0;
+      uint32_t zero_size = P_hdr.p_memsz - P_hdr.p_filesz;
+      for (; i < zero_size;i += PGSIZE){
+        void* paddr = new_page(1);
+        uint32_t sz = (zero_size - i >= PGSIZE) ? PGSIZE : (zero_size - i);
+        _map(&pcb->as, vaddr + i, paddr, 0);
+        memset(paddr, 0, sz);
+      }
+      pcb->max_brk = PGROUNDUP((uint32_t)vaddr + i);
     }
-    fs_close(fd);
-    return E_hdr.e_entry;
+  }
+  fs_close(fd);
+  return E_hdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
