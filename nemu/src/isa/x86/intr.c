914f3cd63cb87fb4175b5dbb7baf2f60980ded30
tracer-ics2019 <tracer@njuics.org>
2020-05-24 09:52:10 +0000

    >  compile 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  09:52:10 up  3:10,  1 user,  load average: 0.10, 0.11, 0.08 802ab7245e1abdb2f4e0a832e03f777ee7040694

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -11,7 +11,8 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   rtl_push((rtlreg_t *)&cpu.eflags.value);
   rtl_push((rtlreg_t *)&cpu.cs);
   rtl_push((rtlreg_t *)&ret_addr);
-  cpu.eflags.IF=0;
+  if(NO==IRQ_TIMER)
+    cpu.eflags.IF = 0;
   uint32_t low = vaddr_read(cpu.idtr.base + 8 * NO, 4);
   uint32_t high = vaddr_read(cpu.idtr.base + 8 * NO + 4, 4);
   uint32_t offset = (low & 0x0000ffff) | (high & 0xffff0000);
