1610c3a40c86ebcce4bdb1b5d946efa02427ccfe
tracer-ics2019 <tracer@njuics.org>
2020-05-24 07:26:59 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  07:26:59 up 45 min,  1 user,  load average: 0.20, 0.08, 0.04 801e64e8335288b6a7666b82818c412b8d144748

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -9,6 +9,7 @@ PCB* fg_pcb = &pcb[1];
 
 void naive_uload(PCB *pcb, const char *filename);
 void context_kload(PCB* pcb, void* entry);
+void context_uload(PCB* pcb, const char* filename);
 
 void switch_boot_pcb() {
   current = &pcb_boot;
@@ -24,7 +25,9 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_kload(&pcb[0], (void*)hello_fun);
+  // context_kload(&pcb[0], (void*)hello_fun);
+  context_uload(&pcb[1], "/bin/init");
+
   switch_boot_pcb();
 
   Log("Initializing processes...");
@@ -37,6 +40,7 @@ int cnt = 0;
 _Context* schedule(_Context *prev) {
   // return NULL;
   current->cp = prev;
-  current = &pcb[0];
+  // current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current->cp;
 }
