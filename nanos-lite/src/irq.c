115caa8c0de0c8b3902010cea21e016fdbfe588c
tracer-ics2019 <tracer@njuics.org>
2020-05-24 09:56:42 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  09:56:42 up  3:15,  1 user,  load average: 0.16, 0.13, 0.09 cc2658985bde574454f2e6928152384726336eb5

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -8,7 +8,7 @@ static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_YIELD: return schedule(c);
     case _EVENT_SYSCALL: return do_syscall(c); break;
-    case _EVENT_IRQ_TIMER: Log("Timer interrupt"); /*_yield();*/break;
+    case _EVENT_IRQ_TIMER: Log("Timer interrupt"); _yield();break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
