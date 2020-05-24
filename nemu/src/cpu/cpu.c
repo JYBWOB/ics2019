e1e1d857e5dc814463e833be643aec2605d9e882
tracer-ics2019 <tracer@njuics.org>
2020-05-24 09:47:50 +0000

    >  compile 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  09:47:50 up  3:06,  1 user,  load average: 0.14, 0.09, 0.05 ea7453a6c654cb6af7f01cba45f3a56ced8b142e

--- nemu/src/cpu/cpu.c
+++ nemu/src/cpu/cpu.c
@@ -1,5 +1,7 @@
 #include "cpu/exec.h"
 
+bool isa_query_intr(void);
+
 CPU_state cpu;
 
 rtlreg_t s0, s1, t0, t1, ir;
@@ -16,7 +18,8 @@ void isa_exec(vaddr_t *pc);
 vaddr_t exec_once(void) {
   decinfo.seq_pc = cpu.pc;
   isa_exec(&decinfo.seq_pc);
-  update_pc();
+  if (isa_query_intr())
+    update_pc();
 
   return decinfo.seq_pc;
 }
