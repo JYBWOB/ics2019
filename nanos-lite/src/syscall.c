0d6800649178bd1a324bd0f3f422dc466c20c189
tracer-ics2019 <tracer@njuics.org>
2020-05-24 08:23:36 +0000

    >  run 1710218 jyb Linux 158a11705f91 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 GNU/Linux  08:23:36 up  1:42,  1 user,  load average: 0.10, 0.07, 0.02 67b9caeed74175af82daaf0c9e737a3e4f52a5fe

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -27,7 +27,8 @@ _Context* do_syscall(_Context *c) {
   a[3] = c->GPR4;
 
   switch (a[0]) {
-    case SYS_exit: sys_execve("/bin/init", NULL, NULL); break;
+    // case SYS_exit: sys_execve("/bin/init", NULL, NULL); break;
+    case SYS_exit: _halt(0); c->GPRx = 0; break;
     case SYS_yield: _yield(); c->GPRx = 0; break;
     case SYS_open: c->GPRx = fs_open((void *)a[1], a[2], a[3]); break;
     case SYS_read: c->GPRx = fs_read(a[1], (void *)a[2], a[3]); break;
